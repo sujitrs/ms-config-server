f2b5475882b41c7501045ee449e27bb610b475c0
sujitrs <suryawanshisujeet@gmail.com>
2019-01-23 15:55:59 +0530

    init

---
+++ src/main/java/org/sj/msconfigserver/MsConfigServerApplication.java
@@ -0,0 +1,14 @@
+package org.sj.msconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsConfigServerApplication.class, args);
+	}
+
+}
+
