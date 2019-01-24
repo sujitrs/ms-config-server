58313a31bf885197bdb35e1574e26cf24d118bfe
sujitrs <suryawanshisujeet@gmail.com>
2019-01-24 14:47:30 +0530

    running

--- src/main/java/org/sj/msconfigserver/MsConfigServerApplication.java
+++ src/main/java/org/sj/msconfigserver/MsConfigServerApplication.java
@@ -1,9 +1,13 @@
 package org.sj.msconfigserver;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
+@EnableAutoConfiguration
 public class MsConfigServerApplication {
 
 	public static void main(String[] args) {
